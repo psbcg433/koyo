6b057310d8625049cb604212eb4cde296bef371d
Prashnik <thebrahmosztech43@gmail.com>
2025-09-02 13:47:39 +0530

    Database connection done

---
+++ backend/src/config/conn.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../utils/constants.js";
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log(
+            `\n🔗 ================================\n` +
+            `✅ MongoDB Connected!\n` +
+            `   🖥️  Host: ${conn.connection.host}\n` +
+            `   📂 Database: ${conn.connection.name}\n` +
+            `==================================\n`
+        );
+    } catch (error) {
+        console.error(
+            `\n❌ ================================\n` +
+            ` MongoDB Connection Failed!\n` +
+            ` Error: ${error.message}\n` +
+            `==================================\n`
+        );
+        process.exit(1);
+    }
+};
+
+
+
+export default connectDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
+import connectDB from "./config/conn.js";
+
+// Load ENV
+console.log("\n================= 🌱 Dotenv Loaded =================");
 
 
 const PORT = process.env.PORT || 5000;
 
 async function startServer(port) {
+  try {
+    await connectDB();
 
-    await app.listen(port, () => {
-        console.log(`Server is running on port ${PORT}`);
+    app.listen(port, () => {
+      console.log("🚀 ===============================================");
+      console.log(`✅  Server is running on:  http://localhost:${PORT}`);
+      console.log("🚀 ===============================================\n");
     });
+  } catch (error) {
+    console.error("\n❌ Error starting server:", error.message, "\n");
+    process.exit(1);
+  }
 }
-
 
 startServer(PORT);
