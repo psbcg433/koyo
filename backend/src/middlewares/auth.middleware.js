d5ffd077c7ba7876956ec0501a05f87ffab36719
Prashnik <thebrahmosztech43@gmail.com>
2025-09-05 22:19:29 +0530

    updated logout route

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,17 +2,29 @@ import { verifyToken } from "../utils/helper.js";
 import { APIError } from "../utils/helperClasses.js";
 
 export const auth = async (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+  const authHeader = req.headers["authorization"];
 
-    if (!token) {
-        throw new APIError("Bearer Token Missing", 401)
-    }
-    try {
-        const decoded = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
-        req.user = decoded; 
-        next();
-    } catch (err) {
-        throw new APIError("Invalid or expired access token", 403);
-    }
-}
+  
+  if (!authHeader) {
+    throw new APIError("Authorization header missing", 401);
+  }
+
+  
+  const parts = authHeader.split(" ");
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    throw new APIError("Authorization header must be in the format: Bearer <token>", 401);
+  }
+
+  const token = parts[1];
+  if (!token) {
+    throw new APIError("Bearer token missing", 401);
+  }
+
+  try {
+    const decoded = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    throw new APIError("Invalid or expired access token", 403);
+  }
+};
--- backend/src/routes/user/user.routes.js
+++ backend/src/routes/user/user.routes.js
@@ -1,12 +1,13 @@
 import {Router} from 'express';
 import UserController from './user.controller.js';
 import {upload} from '../../middlewares/multer.middleware.js';
+import { auth } from '../../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
 userRouter.post('/register',upload.single('avatar'), UserController.registerUser);
 userRouter.post('/login',UserController.loginUser)
 userRouter.get('/refresh',UserController.refreshAccessToken)
-userRouter.post('/logout',UserController.logoutUser)
+userRouter.post('/logout',auth,UserController.logoutUser)
 
 export default userRouter;
